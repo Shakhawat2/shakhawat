e409bfc614e69380e4d2fa65e31eab5d8b856bc0
Shakhawat2 <shohugmahbub@gmail.com>
2023-01-08 18:54:18 +0600

    added some sections

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import Navbar from '../Components/Header/Navbar/Navbar'
+import Footer from '../Components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <Footer></Footer>   
     </>
   )
 }
